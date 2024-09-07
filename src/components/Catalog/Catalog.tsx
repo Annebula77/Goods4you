c070abeee8ac5ede1004a32d5125e27af60b19eb
goharvard77 <goharvard77@gmail.com>
2024-09-07 10:09:53 +0300

    fix flickering of login page

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -2,8 +2,15 @@ import Banner from '../Banner/Banner';
 import GoodsList from '../GoodsList/GoodsList';
 import FAQList from '../FAQList/FAQList';
 import styles from './catalog.module.css';
+import { useUser } from '../../utils/context/useUser';
+import Loader from '../Loader/Loader';
 
 const Catalog = () => {
+  const { user, userIsLoading } = useUser();
+
+  if (!user) return;
+
+  if (userIsLoading) return <Loader />;
   return (
     <section className={styles.catalogContainer}>
       <Banner />

e414201c435e83127e9617c249d06e4030e13b68
goharvard77 <goharvard77@gmail.com>
2024-09-27 15:42:53 +0300

    fix noMatchComponent

--- src/components/NoMatchComponent/NoMatchComponent.tsx
+++ src/components/NoMatchComponent/NoMatchComponent.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import styles from './NoMatchComponent.module.css';
+import styles from './noMatchComponent.module.css';
 
 const NoMatchComponent = () => {
   return (
